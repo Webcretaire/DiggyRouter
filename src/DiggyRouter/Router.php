dc79624a38358a2d4b83f5a3a80cfec1e307b0b7
Julien EMMANUEL <julien.emmanuel@fiducial.net>
2017-07-21 11:36:39 +0200

    Create Route and use action in Router

--- src/DiggyRouter/Router.php
+++ src/DiggyRouter/Router.php
@@ -1,6 +1,8 @@
 <?php
 namespace DiggyRouter;
 
+use Symfony\Component\Yaml\Yaml;
+
 /**
  * Class Router
  *
@@ -9,5 +11,46 @@ namespace DiggyRouter;
  */
 class Router
 {
+    private $routingFile;
+    private $routingData;
+    private $defaultValues = ['action' => 'render'];
 
+    /**
+     * @param string $routingFile
+     */
+    public function loadRoutes($routingFile)
+    {
+        $this->routingFile = $routingFile;
+        $this->routingData = Yaml::parse(file_get_contents($routingFile));
+    }
+
+    /**
+     * @param string $uri
+     * @return bool
+     */
+    public function matchRequest($uri)
+    {
+        $key = array_search($uri, array_column($this->routingData['routes'], 'uri'));
+        if($key !== false)
+        {
+            $route = new Route($this->routingData['routes'][$key]);
+            $this->useRoute($route);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param Route $route
+     */
+    private function useRoute($route)
+    {
+        $controller = new ($route->getController())();
+        $action = $route->getAction();
+        $toPerform = !is_null($action) ? $action->getAction() : $this->defaultValues['action'];
+
+        if(!is_null($action)) {
+            $controller->$toPerform();
+        }
+    }
 }
